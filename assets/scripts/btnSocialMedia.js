d9897ba63c5b0879763d38e9dd6cc1efb8b09295
leon-rdo <leonardoabreu1212@gmail.com>
2022-11-29 19:12:22 -0300

    Elements added to Index.htm
    ### More Elements added to `Index.htm`
    * Social media button group added;
    * `btnSocialMedia.js` created;
    * Script to use CloneNode in the btn-group added to `btnSocialMedia.js` ;
    * Sections to biography, formation and certifications added;

---
+++ assets/scripts/btnSocialMedia.js
@@ -0,0 +1,20 @@
+let btnSocialMediaJSON = [
+    {name: 'LinkedIn', link: 'https://www.linkedin.com/in/leonrdo-costa', icon: '/assets/images/LinkedInLogo.svg'},
+    {name: 'GitHub', link: 'https://github.com/leon-rdo', icon: '/assets/images/GitHubLogo.svg'},
+    {name: 'Instagram', link: 'https://www.instagram.com/leonrdo_mtheus', icon: '/assets/images/InstagramLogo.svg'},
+    {name: 'Twitter', link: 'https://twitter.com/leonrdomtheus', icon: '/assets/images/TwitterLogo.svg'},
+    {name: 'Koo', link: 'https://www.kooapp.com/profile/leonardoabreu', icon: '/assets/images/KooLogo.svg'},
+    //{name: '', link: '', icon: '/assets/images/'},
+];
+
+let btnSocialMediaArea = document.querySelector('.Box-RedesSociais');
+
+for (let i = 0; i < btnSocialMediaJSON.length; i++) {
+
+    let itemSocialMediaBtn = document.querySelector('.modelo-btn').cloneNode(true);
+    itemSocialMediaBtn.querySelector('.modelo-btn .RedeSocialLogo').src = btnSocialMediaJSON[i].icon;
+    itemSocialMediaBtn.querySelector('.modelo-btn .RedeSocialLogo').alt = 'ícone do ' + btnSocialMediaJSON[i].name;
+    itemSocialMediaBtn.href = btnSocialMediaJSON[i].link;
+
+    btnSocialMediaArea.append(itemSocialMediaBtn);
+}
