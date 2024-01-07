7636b718c38272a8c3aa02af5b040402014d2418
Leonardo Costa <leonardoabreu1212@gmail.com>
2024-01-07 18:19:07 -0300

    feat: register models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Settings)
+class SettingsAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(SocialMedia)
+class SocialMediaAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Education)
+class EducationAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Testimonial)
+class TestimonialAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    pass
