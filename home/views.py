44929f176db598071b1a5f10d54c4bbb323db4e4
Leonardo Costa <leonardoabreu1212@gmail.com>
2024-01-25 21:51:30 -0300

    feat: make template texts changeble

--- home/views.py
+++ home/views.py
@@ -9,5 +9,12 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["about_text"] = Text.objects.filter(spot='about').first()
+        context["facts_text"] = Text.objects.filter(spot='facts').first()
+        context["skills_text"] = Text.objects.filter(spot='skills').first()
+        context["resume_text"] = Text.objects.filter(spot='resume').first()
+        context["portfolio_text"] = Text.objects.filter(spot='portfolio').first()
+        context["services_text"] = Text.objects.filter(spot='services').first()
+        context["testimonials_text"] = Text.objects.filter(spot='testimonials').first()
+        context["contact_text"] = Text.objects.filter(spot='contact').first()
         return context
     
