ae6369f6e50280f94bdab8a99f975aea7b751828
Leonardo Costa <leonardoabreu1212@gmail.com>
2024-02-23 23:16:24 -0300

    feat: customize admin site header and titles

--- home/admin.py
+++ home/admin.py
@@ -1,33 +1,46 @@
 from django.contrib import admin
 from .models import *
 
+
+admin.site.site_header = "Administração do Site"
+admin.site.site_title = "Administração"
+admin.site.index_title = "Leonardo Costa"
+
+
 @admin.register(Settings)
 class SettingsAdmin(admin.ModelAdmin):
     pass
+
 
 @admin.register(SocialMedia)
 class SocialMediaAdmin(admin.ModelAdmin):
     pass
 
+
 @admin.register(Education)
 class EducationAdmin(admin.ModelAdmin):
     pass
+
 
 @admin.register(Experience)
 class ExperienceAdmin(admin.ModelAdmin):
     pass
 
+
 @admin.register(Skill)
 class SkillAdmin(admin.ModelAdmin):
     pass
+
 
 @admin.register(Portfolio)
 class PortfolioAdmin(admin.ModelAdmin):
     pass
 
+
 @admin.register(Text)
 class TextAdmin(admin.ModelAdmin):
     pass
+
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
