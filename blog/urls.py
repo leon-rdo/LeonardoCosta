2cec485126006df6583b71cedccdec1178616c81
Leonardo Costa <leonardoabreu1212@gmail>
2024-08-31 22:49:29 -0300

    fix: reorder urls to avoid error

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from .views import BlogIndexView, PostCreateView, PostDetailView
 app_name = 'blog'
 urlpatterns = [
     path('', BlogIndexView.as_view(), name='index'),
-    path('publicacao/<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
     path('publicacao/nova/', PostCreateView.as_view(), name='post_create'),
+    path('publicacao/<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
 ]
