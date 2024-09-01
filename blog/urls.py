4dac114b6ad3ee511f7c8e37b2efbe6e031dbbe5
Leonardo Costa <leonardoabreu1212@gmail>
2024-08-31 22:46:25 -0300

    refactor: change url to portuguese

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from .views import BlogIndexView, PostCreateView, PostDetailView
 app_name = 'blog'
 urlpatterns = [
     path('', BlogIndexView.as_view(), name='index'),
-    path('post/<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
-    path('post/new/', PostCreateView.as_view(), name='post_create'),
+    path('publicacao/<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
+    path('publicacao/nova/', PostCreateView.as_view(), name='post_create'),
 ]
